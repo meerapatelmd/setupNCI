51c8b02a17cdecf1579d139c69ba754aedf2843e
Meera Patel <patelmeeray@gmail.com>
2021-03-25 18:47:53 -0400

    deploy pkg using glitter

--- R/dat.R
+++ R/dat.R
@@ -3,14 +3,14 @@
 
 
 
-dat_data <- readLines("~/Desktop/Metathesaurus.RRF/META/nci_code_cui_map_202008.dat")
-dat_data <-
-        readr::read_delim(
-                file = "~/Desktop/Metathesaurus.RRF/META/nci_code_cui_map_202008.dat",
-                delim = "|",
-                col_names = c("code",
-                              "cui",
-                              "concept_name",
-                              "str",
-                              "empty")
-        )
+# dat_data <- readLines("~/Desktop/Metathesaurus.RRF/META/nci_code_cui_map_202008.dat")
+# dat_data <-
+#         readr::read_delim(
+#                 file = "~/Desktop/Metathesaurus.RRF/META/nci_code_cui_map_202008.dat",
+#                 delim = "|",
+#                 col_names = c("code",
+#                               "cui",
+#                               "concept_name",
+#                               "str",
+#                               "empty")
+#         )
