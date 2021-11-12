8b802b6ccc91e66935d1f347a6e0914c75199656
Meera <mpatel@concertai.com>
2021-11-12 18:20:36 -0500

    update comments

--- inst/sql/nci_mrhier.sql
+++ inst/sql/nci_mrhier.sql
@@ -2,7 +2,7 @@
 * Derive entire hierarchies from NCI Metathesaurus MRHIER Table
 * Authors: Meera Patel
 * Date: 2021-10-27
-* https://lucid.app/lucidchart/b7e40e42-ea80-43be-baf5-7f92cbfb6d6f/edit?viewport_loc=-185%2C997%2C1560%2C929%2C0_0&invitationId=inv_61a38c21-37a1-49fa-a857-d7585471e5f1
+* https://lucid.app/lucidchart/f68b0a0a-104c-4550-bc38-455efda9ee25/edit?viewport_loc=-776%2C-93%2C2719%2C1620%2C0_0&invitationId=inv_e6984473-d151-4be5-8302-86fa64f4865c
 *
 * | MRHIER | --> | MRHIER |
 * ptr_id is added to the source table. ptr_id is the source MRHIER's row number.
@@ -17,19 +17,11 @@
 * `ptr` values in `MRHIER_STR` are accounted for in the `MRHIER_STR_EXCL` table.
 *
 * To Do:
-* [X] Cleanup scripts with functions from 1387 onward, including logging to
-*     the progress log and annotations
-* [ ] After 2021AB update, see how the lookup and results tables can be
-*     renamed with a degree of provenance.
 * [ ] Add the `sab` field back to final MRHIER_STR table by doing a join 
 *     to the MRCONSO table at this stage
-* [X] Some log entries do not have target table row counts
 * [ ] Log entries from `ext_` to `pivot_` do not have `sab` value
-* [X] Re-imagine the RxClass log so that it is a 1-row entry per incidence (mimic setup_nci_class_log)
-* [X] Change sort order of final tables in RxClass 
 * [ ] Change sort order of final tables in NCI Class
 * [ ] Add indexes to final NCI Class tables
-* [X] Add indexes to final RxClass tables
 **************************************************************************/
 
 
