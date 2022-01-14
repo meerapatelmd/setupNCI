7cbd01ea1bf3dce67652dba289f431bf11dfda7b
Meera Patel <patelmeeray@gmail.com>
2022-01-14 17:54:57 -0500

    test setting options using settings package

---
+++ R/pkg_options.R
@@ -0,0 +1,32 @@
+# Variable, global to package's namespace.
+# This function is not exported to user space and does not need to be documented.
+MYPKGOPTIONS <-
+  options_manager(
+    output_folder=NULL
+  )
+
+
+#' Set or get options for my package
+#'
+#' @param ... Option names to retrieve option values or \code{[key]=[value]} pairs to set options.
+#'
+#' @section Supported options:
+#' The following options are supported
+#' \itemize{
+#'  \item{\code{output_folder}}{(\code{character};1) Folder to which outputs for data processing will be written to. }
+#' }
+#'
+#' @export
+pkg_options <- function(...){
+  # protect against the use of reserved words.
+  stop_if_reserved(...)
+  MYPKGOPTIONS(...)
+}
+
+
+#' Reset global options for pkg
+#'
+#' @export
+pkg_reset() reset(MYPKGOPTIONS)
+
+
