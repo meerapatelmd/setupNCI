7ed20055e05d275bdedcd61235b7e72a66197d9a
Meera <mpatel@concertai.com>
2022-06-13 18:01:51 -0400

    convert ddl from integer to bigint to accomodate 7-billion concept id values

--- R/setup_omop.R
+++ R/setup_omop.R
@@ -90,7 +90,7 @@ setup_omop <-
             "
               --HINT DISTRIBUTE ON RANDOM
               CREATE TABLE {schema}.concept (
-                concept_id			INTEGER			NOT NULL ,
+                concept_id			BIGINT			NOT NULL ,
                 concept_name			text	NOT NULL ,
                 domain_id				VARCHAR(20)		NOT NULL ,
                 vocabulary_id			VARCHAR(20)		NOT NULL ,
@@ -108,27 +108,27 @@ setup_omop <-
                 vocabulary_name		VARCHAR(255)	NOT NULL,
                 vocabulary_reference	VARCHAR(255)	NOT NULL,
                 vocabulary_version	VARCHAR(255)	NULL,
-                vocabulary_concept_id	INTEGER
+                vocabulary_concept_id	BIGINT
               )
               ;
               --HINT DISTRIBUTE ON RANDOM
               -- CREATE TABLE {schema}.domain (
               --  domain_id			    VARCHAR(20)		NOT NULL,
               --  domain_name		    VARCHAR(255)	NOT NULL,
-              --  domain_concept_id		INTEGER			NOT NULL
+              --  domain_concept_id		BIGINT			NOT NULL
               -- )
               -- ;
               --HINT DISTRIBUTE ON RANDOM
               CREATE TABLE {schema}.concept_class (
                 concept_class_id			VARCHAR(20)		NOT NULL,
                 concept_class_name		VARCHAR(255)	NOT NULL,
-                concept_class_concept_id	INTEGER
+                concept_class_concept_id	BIGINT
               )
               ;
               --HINT DISTRIBUTE ON RANDOM
               CREATE TABLE {schema}.concept_relationship (
-                concept_id_1			INTEGER			NOT NULL,
-                concept_id_2			INTEGER			NOT NULL,
+                concept_id_1			BIGINT			NOT NULL,
+                concept_id_2			BIGINT			NOT NULL,
                 relationship_id		VARCHAR(255)		NOT NULL,
                 valid_start_date		DATE			NOT NULL,
                 valid_end_date		DATE			NOT NULL,
@@ -142,20 +142,20 @@ setup_omop <-
                 is_hierarchical			VARCHAR(1)		NOT NULL,
                 defines_ancestry			VARCHAR(1)		NOT NULL,
                 reverse_relationship_id	VARCHAR(20)	,
-                relationship_concept_id	INTEGER
+                relationship_concept_id	BIGINT
               )
               ;
               --HINT DISTRIBUTE ON RANDOM
               CREATE TABLE {schema}.concept_synonym (
-                concept_id			INTEGER			NOT NULL,
+                concept_id			BIGINT			NOT NULL,
                 concept_synonym_name	text	NOT NULL,
-                language_concept_id	INTEGER			NOT NULL
+                language_concept_id	BIGINT			NOT NULL
               )
               ;
               --HINT DISTRIBUTE ON RANDOM
               CREATE TABLE {schema}.concept_ancestor (
-                ancestor_concept_id		INTEGER		NOT NULL,
-                descendant_concept_id		INTEGER		NOT NULL,
+                ancestor_concept_id		BIGINT		NOT NULL,
+                descendant_concept_id		BIGINT		NOT NULL,
                 min_levels_of_separation	INTEGER		NOT NULL,
                 max_levels_of_separation	INTEGER		NOT NULL
               )
